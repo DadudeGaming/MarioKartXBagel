daa5e3bfeb382f50c6032fc9ab337be3aa4795b1
Pixel-Bat <sebbraun007@gmail.com>
2025-01-24 08:28:19 -0500

    Reapply "Set up advantage kit?"
    This reverts commit 9532b47a685e032a7479ed1921aed4b1e044dd06.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,8 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+
+  public static class CAN_IDS {
+    public static final int PDH_ID = 10;
+  }
 }
