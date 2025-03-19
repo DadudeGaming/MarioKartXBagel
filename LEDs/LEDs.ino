037cef5e5794220a67f2b2f9988f96686914992b
C Yo <young.carter@outlook.com>
2025-03-19 18:00:22 -0400

    added fading between colours

--- LEDs/LEDs.ino
+++ LEDs/LEDs.ino
@@ -5,6 +5,7 @@ int powerPin = 3, rPin = 9, gPin = 10, bPin = 11;
 String gold = "#FFD700";
 String green = "#61B077";
 String red = "#FF0000";
+String white = "#FFFFFF";
 String currentColour = "";
 
 bool power = false;
@@ -12,6 +13,14 @@ bool power = false;
 int numberCode = -1;
 
 unsigned long previousMillis = 0;
+
+bool fading = false;
+String fadeFrom;
+String fadeTo;
+int fadeSteps = 0;
+int currentFadeStep = 0;
+unsigned long fadeStepDelay = 0;
+unsigned long fadeLastUpdate = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -34,10 +43,11 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available()) {
     String serialString = Serial.readString();
+    numberCode = serialString.toInt();
   }
 
   if (numberCode == 0) {
-    //disable
+    Disabled();
   } else if (numberCode == 1) {
     TeleOp();
   } else if (numberCode == 2) {
@@ -81,6 +91,62 @@ void setColour(String colour) {
   analogWrite(bPin, rgb[2]);
 }
 
+void startFade(String fromColour, String toColour, int steps, int stepDelay) {
+  fadeFrom = fromColour;
+  fadeTo = toColour;
+  fadeSteps = steps;
+  fadeStepDelay = stepDelay;
+  currentFadeStep = 0;
+  fadeLastUpdate = millis();
+  fading = true;
+}
+
+// This function should be called frequently (in loop) to update the fade.
+void updateFade() {
+  if (!fading) return;
+
+  if (millis() - fadeLastUpdate >= fadeStepDelay) {
+    
+    long fromNumber = strtol(fadeFrom.substring(1).c_str(), NULL, 16);
+    int fromR = fromNumber >> 16;
+    int fromG = (fromNumber >> 8) & 0xFF;
+    int fromB = fromNumber & 0xFF;
+
+    long toNumber = strtol(fadeTo.substring(1).c_str(), NULL, 16);
+    int toR = toNumber >> 16;
+    int toG = (toNumber >> 8) & 0xFF;
+    int toB = toNumber & 0xFF;
+
+    float factor = (float)currentFadeStep / fadeSteps;
+    int currentR = fromR + (toR - fromR) * factor;
+    int currentG = fromG + (toG - fromG) * factor;
+    int currentB = fromB + (toB - fromB) * factor;
+
+    int rgb[3] = { currentR, currentG, currentB };
+    for (int i = 0; i < 3; i++) {
+      if (rgb[i] <= 0) {
+        rgb[i] = 255;
+      } else {
+        rgb[i] = -rgb[i];
+      }
+    }
+
+    digitalWrite(powerPin, HIGH);
+    power = true;
+    analogWrite(rPin, rgb[0]);
+    analogWrite(gPin, rgb[1]);
+    analogWrite(bPin, rgb[2]);
+
+    currentFadeStep++;
+    fadeLastUpdate = millis();
+
+
+    if (currentFadeStep > fadeSteps) {
+      fading = false;
+      currentColour = fadeTo;
+    }
+  }
+}
 void TeleOp() {
   setColour(gold);
   if (millis() - previousMillis >= 700) {
@@ -134,7 +200,16 @@ void EStop() {
 }
 
 void Disabled() {
-  // fade between colours
+  updateFade();
+  if (currentColour.equals(gold)) {
+    startFade(gold, green, 20, 500);
+  }
+  if (currentColour.equals(green)) {
+    startFade(green, white, 20, 500);
+  }
+  if (currentColour.equals(white)) {
+    startFade(white, gold, 20, 500);
+  }
 }
 
 void ClimbCorrectAngle() {
